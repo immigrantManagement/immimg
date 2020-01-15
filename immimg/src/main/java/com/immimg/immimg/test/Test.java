201ba6854243c2b49cb435295fcf09c62709a219
张家斌 <zhangjiabinlwsb@gmail.com>
2020-01-15 18:39:41 +0800

    张家斌测试

---
+++ immimg/src/main/java/com/immimg/immimg/test/Test.java
@@ -0,0 +1,11 @@
+package com.immimg.immimg.test;
+
+/**
+ * @author lenovo
+ * @version V1.0
+ * @Project immigrantManagement
+ * @Package com.immimg.immimg.test
+ * @date 2020/1/15 18:38 星期三
+ */
+public class Test {
+}
