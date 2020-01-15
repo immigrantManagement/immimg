73067bd5d087919ddda471873a2d05d95f8f17a2
张家斌 <zhangjiabinlwsb@gmail.com>
2020-01-15 20:58:31 +0800

    张家斌测试

---
+++ immimg/src/main/java/com/immimg/immimg/model/entity/News.java
@@ -0,0 +1,13 @@
+package com.immimg.immimg.model.entity;
+
+/**
+ * @author lenovo
+ * @version V1.0
+ * @Project immigrantManagement
+ * @Package com.immimg.immimg.model.entity
+ * @date 2020/1/15 20:54 星期三
+ */
+public class News {
+    //新闻id
+    //新闻内容
+}
