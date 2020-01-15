9517b16dd9fca643c89af3ca89495d31a6eee668
nlqiong <Nlqiong@163.com>
2020-01-15 10:07:13 +0800

    创建项目

---
+++ immimg/src/main/java/com/immimg/immimg/ImmimgApplication.java
@@ -0,0 +1,13 @@
+package com.immimg.immimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmimgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmimgApplication.class, args);
+    }
+
+}
---
+++ immimg/src/test/java/com/immimg/immimg/ImmimgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.immimg.immimg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImmimgApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
